f247132e512cc33d8d4cb9ff96be14c5dfa4dfd7
Bachibouzouk <pierre-francois.duc@rl-institut.de>
2019-05-17 12:24:30 +0200

    Remove unused code from intro layout

--- app_layouts/intro_layout.py
+++ app_layouts/intro_layout.py
@@ -56,10 +56,3 @@ layout = html.Div(
         )
     ]
 )
-
-
-@app.callback(
-    Output('app-1-display-value', 'children'),
-    [Input('app-1-dropdown', 'value')])
-def display_value(value):
-    return 'You have selected "{}"'.format(value)
