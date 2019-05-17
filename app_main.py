0ccd3a58c8bcb6265ee8e26d3c7bf6b3e23563fa
Bachibouzouk <pierre-francois.duc@rl-institut.de>
2019-05-17 14:09:22 +0200

    Update CSS for the app and load the template from Bryan

--- app_main.py
+++ app_main.py
@@ -5,6 +5,13 @@ URL_BASEPATH = 'NDC-visualization'
 # Initializes dash app
 app = dash.Dash(__name__)
 
+# Load css file
+external_css = [
+    'https://raw.githack.com/rl-institut/WAM/dev/static/foundation/css/app.css',
+]
+for css in external_css:
+    app.css.append_css({"external_url": css})
+
 app.title = 'NDC visualisation'
 
 server = app.server
