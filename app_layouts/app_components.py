926c7a076754e58ed642e7c6e113b2c3612e87b7
pfduc <pierre-francois.duc@rl-institute.de>
2019-05-21 20:37:53 +0200

    Add yaxis choice for the compare barplot

--- app_layouts/app_components.py
+++ app_layouts/app_components.py
@@ -187,7 +187,6 @@ def results_div(aggregate=False):
         html.Div(
             id='{}-barplot-title'.format(id_name),
             children=[
-                html.P('HERE GRAPH TITLE, for example "% pop. newly electrified in 2030"'),
                 dcc.Dropdown(
                     id='{}-barplot-yaxis-input'.format(id_name),
                     options=[{'label': r, 'value': r} for r in BASIC_ROWS],
@@ -343,7 +342,13 @@ def compare_div():
     results_divs = [
         html.Div(
             id='{}-barplot-title'.format(id_name),
-            children='HERE GRAPH TITLE, for example "% pop. newly electrified in 2030"',
+            children=[
+                dcc.Dropdown(
+                    id='{}-barplot-yaxis-input'.format(id_name),
+                    options=[{'label': r, 'value': r} for r in BASIC_ROWS],
+                    value=BASIC_ROWS[0],
+                )
+            ],
         ),
         barplot,
         html.Div(
