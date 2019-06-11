d071f21080fbdae4b3868d24067371b5448d34cb
Bryan <bmlancien@yahoo.com>
2019-06-11 16:53:43 +0200

    change h2 to h3

--- app_layouts/intro_layout.py
+++ app_layouts/intro_layout.py
@@ -7,7 +7,7 @@ from app_main import app, URL_BASEPATH, PLACEHOLDER
 from . import static_layout, flex_layout
 
 description_content = [
-    html.H2('Universal Access to Electricity by 2030...'),
+    html.H3('Universal Access to Electricity by 2030...'),
     html.P(['...cannot be achieved by grid extension alone. In 2019, we are still talking about almost 1 billion people worldwide living without access to electricity, most of them are living in rural areas of developing countries. Grid extension is slow, expensive, provides unreliable service, and it increases reliance on fossil-fuel based electricity generation. Grid extension is not a viable answer to the pressing needs of neglected populations.']),
     html.P(['What we need are CLEANER, CHAPER & SMARTER solutions, if we want to ensure that access to electricity is provided sustainably and universally until 2030. And, we need to strengthen international commitment to implement these solutions on a larger scale.']),
     html.P(['This is what Researchers at the ', html.A(children='Reiner Lemoine Institute', href='https://reiner-lemoine-institut.de/en/'), ' (RLI) and the greenwerk have been working on over the last year. The RLI has developed feasible country-level scenarios for the deployment of off-grid renewable energy solutions that mitigate climate change, are more cost-effective than gird extension approaches, and leave no-one behind. The greenwerk’s analysis has shown that country-level realization of these scenarios is further accompanied by significant economic and social dividends to those most in need, which can propel development and build resilience to climate change. ']),
@@ -15,7 +15,7 @@ description_content = [
 ]
 
 about_content = [
-    html.H2('What can this tool do?'),
+    html.H3('What can this tool do?'),
     html.P('Off-grid renewable energy solutions are CLEANER, CHEAPER & SMARTER – this is what our model aims to convey. The Reiner Lemoine Institute and the greenwerk have collected and analysed huge amounts of data from all over the world. This data has been translated into interactive electrification scenarios that showcase feasible deployment pathways through which the universal access goal can be met. You can engage with these scenarios through this tool and alter underlying assumptions. The tool allows you to:'),
     html.P('LEARN about the access gap of a given country and the mix of technology options (Grid Extension, Mini-Grids and Solar-Home-Systems) available to close it.'),
     html.P('COMPARE the initial investment needs and the mitigation potential of different technology options in different scenarios and for different ESMAP TIER level assumptions.'),
