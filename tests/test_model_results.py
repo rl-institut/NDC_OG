2d94da19a085de2078db14828272dacb9a8d4ca6
Bachibouzouk <pierre-francois.duc@rl-institut.de>
2019-09-30 08:50:04 +0200

    Add tests for scenario results

--- tests/test_model_results.py
+++ tests/test_model_results.py
@@ -15,10 +15,16 @@ from data.data_preparation import (
     HH_GET,
     HH_CAP,
     HH_SCN2,
+    GHG,
+    GHG_CAP,
     compute_ndc_results_from_raw_data
 )
 
-COMP_COLS = POP_GET + HH_GET + HH_CAP + HH_SCN2
+COMP_EXO = POP_GET + HH_GET + HH_CAP + HH_SCN2
+COMP_GHG = GHG + GHG_CAP
+
+WORLD_ID = 'WD'
+REGIONS_NDC = dict(WD=['LA', 'SSA', 'DA'], SA='LA', AF='SSA', AS='DA')
 
 
 SCENARIOS_DATA = {
@@ -26,71 +32,92 @@ SCENARIOS_DATA = {
 }
 
 
-def highlight_mismatch(col, df_diff,eps=0.001):
+def highlight_mismatch(col=None, df_diff=None, eps=0.001):
+    """Filter lines of a Dataframe column which are larger than threshold"""
     return df_diff.loc[np.abs(df_diff[col]) > eps]
 
 
 class TestModellass(unittest.TestCase):
 
-    def test_bau_exo_results(self):
-        bau_df = pd.read_json(SCENARIOS_DATA[BAU_SCENARIO]).set_index('country_iso').sort_index(
-            ascending=True)
-        xls_bau = pd.read_csv('data/xls_bau.csv', float_precision='high')
-
-        df_diff = xls_bau[COMP_COLS] - bau_df[COMP_COLS]
+    def compare_results_countries(self, sce, fname, comp, eps=0.001):
+        df = pd.read_json(SCENARIOS_DATA[sce]).set_index('country_iso')
+        df = df.sort_index(ascending=True)
+        xls = pd.read_csv('tests/data/{}'.format(fname), float_precision='high').set_index('country_iso')
+        xls = xls.sort_index(ascending=True)
+        df_diff = xls[comp] - df[comp]
 
         l = []
-        for col in COMP_COLS:
-            temp = highlight_mismatch(col, df_diff).index.to_list()
+        for col in comp:
+            temp = highlight_mismatch(col, df_diff, eps).index.to_list()
             if temp:
                 print('problems with ', col, temp)
                 print(len(temp))
             l = l + temp
-        len(set(l))
         l = list(set(l))
-        df_diff.loc[l]
 
         self.assertEqual(len(set(l)), 0)
 
-    def test_uea_exo_results(self):
-        se_df = pd.read_json(SCENARIOS_DATA[SE4ALL_SCENARIO]).set_index('country_iso').sort_index(
-            ascending=True)
-        xls_se = pd.read_csv('data/xls_se.csv', float_precision='high').set_index(
-            'country_iso').sort_index(ascending=True)
+    def compare_results_region(self, sce, region_id, fname, comp, eps=0.001):
+        df = pd.read_json(SCENARIOS_DATA[sce]).set_index('country_iso')
+        df = df.sort_index(ascending=True)
+        xls = pd.read_csv('tests/data/{}'.format(fname), float_precision='high').set_index('country_iso')
+        xls = xls.sort_index(ascending=True)
 
-        df_diff = xls_se[COMP_COLS] - se_df[COMP_COLS]
+        if region_id != WORLD_ID:
+            df = df.loc[df.region == REGIONS_NDC[region_id]]
+            xls = xls.loc[df.region == REGIONS_NDC[region_id]]
+
+        df_diff = xls[comp].sum(axis=0) - df[comp].sum(axis=0)
 
         l = []
-        for col in COMP_COLS:
-            temp = highlight_mismatch(col, df_diff).index.to_list()
+        for col in comp:
+            temp = highlight_mismatch(col, df_diff, eps).index.to_list()
             if temp:
                 print('problems with ', col, temp)
                 print(len(temp))
             l = l + temp
-        len(set(l))
         l = list(set(l))
-        df_diff.loc[l]
 
-        self.assertEqual(len(set(l)), 0)
+        self.assertEqual(len(l), 0)
+
+    def test_bau_exo_results_countries(self):
+        self.compare_results_countries(
+            BAU_SCENARIO,
+            'NDC_full_results_old_rise_model_{}.csv'.format(BAU_SCENARIO),
+            COMP_EXO,
+            0.2
+        )
+
+    def test_bau_exo_results_region(self):
+        self.compare_results_region(
+            BAU_SCENARIO,
+            WORLD_ID,
+            'NDC_full_results_old_rise_model_{}.csv'.format(BAU_SCENARIO),
+            COMP_EXO,
+            0.2
+        )
+
+    def test_bau_ghg_results(self):
+        self.compare_results(BAU_SCENARIO, 'ghg_{}.csv'.format(BAU_SCENARIO), COMP_GHG)
 
     def test_prog_exo_results(self):
-        xls_prog = pd.read_csv('data/xls_prog.csv', float_precision='high')
-        prog_df = pd.read_json(SCENARIOS_DATA[PROG_SCENARIO]).set_index(
-            'country_iso').sort_index(ascending=True)
+        self.compare_results_countries(
+            PROG_SCENARIO,
+            'NDC_full_results_old_rise_model_{}.csv'.format(PROG_SCENARIO),
+            COMP_EXO,
+            0.2
+        )
 
-        COMP_COLS = POP_GET + HH_GET + HH_CAP + HH_SCN2
+    def test_prog_ghg_results(self):
+        self.compare_results(PROG_SCENARIO, 'ghg_{}.csv'.format(PROG_SCENARIO), COMP_GHG)
 
-        df_diff = xls_prog[COMP_COLS] - prog_df[COMP_COLS]
+    def test_uea_exo_results(self):
+        self.compare_results_countries(
+            SE4ALL_SCENARIO,
+            'NDC_full_results_old_rise_model_{}.csv'.format(SE4ALL_SCENARIO),
+            COMP_EXO,
+            0.2
+        )
 
-        l = []
-        for col in COMP_COLS:
-            temp = highlight_mismatch(col, df_diff).index.to_list()
-            if temp:
-                print('problems with ', col, temp)
-                print(len(temp))
-            l = l + temp
-        len(set(l))
-        l = list(set(l))
-        df_diff.loc[l]
-
-        self.assertEqual(len(set(l)), 0)
+    def test_uea_ghg_results(self):
+        self.compare_results(SE4ALL_SCENARIO, 'ghg_{}.csv'.format(SE4ALL_SCENARIO), COMP_GHG)
