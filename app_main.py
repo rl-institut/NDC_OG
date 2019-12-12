50b56b8eb56ae9bff1ecb4c1841b6177cdf539c4
Bachibouzouk <pierre-francois.duc@rl-institut.de>
2019-12-12 11:33:15 +0100

    Use foundation css file modified by Bryan

--- app_main.py
+++ app_main.py
@@ -19,13 +19,6 @@ APP_BG_COLOR = '#FFFFFF'
 # Initializes dash app
 app = dash.Dash(__name__)
 
-# Load css file
-external_css = [
-    'https://raw.githack.com/rl-institut/WAM/dev/static/foundation/css/app.css',
-]
-for css in external_css:
-    app.css.append_css({"external_url": css})
-
 app.index_string = '''
     <!DOCTYPE html>
     <html>
